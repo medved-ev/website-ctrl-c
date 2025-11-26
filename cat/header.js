1e68ff1f6a92c8d06ee4dcc80d279742f10f2b65
LeBiterino <biterr@swisscows.email>
2025-11-26 17:28:31 +0100

    more minor fixes

--- cat/header.js
+++ cat/header.js
@@ -1,5 +1,5 @@
 document.write(`\
 \
-<link rel="stylesheet" href="style.css"><a href="https://ctrl-c.club/~biterr/"><button>Home</button></a><a href="https://ctrl-c.club/~biterr/pages/gallery.html"><button>My cat's gallery</button><a/> \
+<link rel="stylesheet" href="style.css"><a href="https://ctrl-c.club/~biterr/"><button>Home</button></a><a href="https://ctrl-c.club/~biterr/cat/pages/gallery.html"><button>My cat's gallery</button><a/> \
 \
 `);
